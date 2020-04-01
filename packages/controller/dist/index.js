634d641c18f022ad5657a6878d35cfa1d6cf9f9e
ganadara135 <ganadara135@gmail.com>
2020-04-02 05:04:49 +0900

    typescript error on LoginMutation

--- packages/controller/dist/index.js
+++ packages/controller/dist/index.js
@@ -4,4 +4,5 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./modules/RegisterController"));
+__export(require("./modules/LoginController"));
 //# sourceMappingURL=index.js.map
